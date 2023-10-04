d27ffcfb7220f8effa736db3650c4054a4b70ef8
Ghostovich <gusarov_konstant@mail.ru>
2023-10-04 09:30:48 +0300

    hw8.1

--- hw8/index.js
+++ hw8/index.js
@@ -18,6 +18,32 @@
 // manager.displayInfo(); // "Name: John Doe, Department: Sales"
 
 
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
+  displayInfo() {
+    console.log(`Name: ${this.name}`);
+  }
+}
+
+const employee = new Employee("John Smith");
+employee.displayInfo(); // "Name: John Smith"
+
+
+class Manager {
+  constructor(department) {
+    this.department = department;
+  }
+  displayInfo() {
+    console.log(`${this.name}, ${this.department}`);
+  }
+  // __proto__: Employee;
+}
+
+const manager = new Manager("Jane Doe", "Sales");
+manager.displayInfo(); // "Name: John Doe, Department: Sales"
+
 // Задание 2: "Управление списком заказов"
 
 // Реализуйте класс Product (товар), который имеет следующие свойства и методы:
@@ -41,3 +67,39 @@
 // order.addProduct(product2);
 
 // console.log(order.getTotalPrice()); // Вывод: 1100
+
+class Product {
+  constructor(name, price, quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+}
+
+// Свойство id (номер заказа) - число, уникальный номер заказа.
+// Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+// Метод addProduct(product) - принимает объект класса Product и добавляет его в список продуктов заказа.
+// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+class Order {
+  constructor(id, products) {
+    this.id = id
+    this.products = products;
+  }
+  addProduct() {
+    listProduct = 0;
+  }
+  getTotalPrice() {
+    return TotalPrice = price;
+  }
+}
+
+const order = new Order(12345);
+
+const product1 = new Product("Phone", 500, 2);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100, 1);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); // Вывод: 1100
