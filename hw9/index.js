3c9fcd5b8ebb686823f4aa0833c5f20305280a8c
Ghostovich <gusarov_konstant@mail.ru>
2023-10-10 00:36:34 +0300

    hw9.2

--- hw9/index.js
+++ hw9/index.js
@@ -9,11 +9,42 @@
 
 // Работа должна быть выполнена с API: https://reqres.in/
 
+function loadData(url) {
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return response.text();
+      }
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+getUserData(ID) {
+  let arr = loadData("https://reqres.in/");
+  if (ID === arr.ID) {
+    return arr.values();
+
+  }
+  else {
+    console.log(error.message);
+  }
+};
+
+console.log(getUserData(12));
 
 
 // Задание 2. Отправка данных на сервер.
 
-// Реализуйте функцию saveUserData, которая принимает объект с данными о пользователе в качестве аргумента и использует fetch для отправки этих данных на удаленный сервер для сохранения. Функция должна возвращать промис, который разрешается, если данные успешно отправлены, или отклоняется в случае ошибки.
+// Реализуйте функцию saveUserData, которая принимает объект с данными о пользователе в качестве аргумента 
+// и использует fetch для отправки этих данных на удаленный сервер для сохранения.
+// Функция должна возвращать промис, который разрешается, если данные успешно отправлены, или отклоняется в случае ошибки.
 
 // *Подсказка *
 
@@ -30,6 +61,32 @@
 //   .catch(error => {
 //     console.log(error.message);
 //   });
-// saveUserData использует fetch для отправки данных о пользователе на удаленный сервер для сохранения. Она отправляет POST-запрос на URL-адрес /api/users с указанием типа содержимого application/json и сериализует объект с данными о пользователе в JSON-строку с помощью JSON.stringify(). Если запрос успешен (с кодом 201), функция разрешает промис. Если запрос неуспешен, функция отклоняет промис с сообщением об ошибке.
+// saveUserData использует fetch для отправки данных о пользователе на удаленный сервер для сохранения. 
+// Она отправляет POST-запрос на URL-адрес /api/users с указанием типа содержимого application/json 
+// и сериализует объект с данными о пользователе в JSON-строку с помощью JSON.stringify(). 
+// Если запрос успешен (с кодом 201), функция разрешает промис. Если запрос неуспешен, функция отклоняет промис с сообщением об ошибке.
 
 // Работа должна быть выполнена с API: https://reqres.in/
+
+
+
+const user = {
+  "name": "John Doe",
+  "job": "unknown"
+};
+
+saveUserData(user, url)
+fetch(url)
+  .then((user) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.send(JSON.stringify(user));
+  })
+  .then(() => {
+    console.log('User data saved successfully');
+  })
+  .catch(error => {
+    console.log(error.message);
+  });
+
+saveUserData(user, "https://reqres.in/");
